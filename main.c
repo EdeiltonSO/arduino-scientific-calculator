fd023deeebedbe29284f2e252b94e20f72e7b219
Edeilton Oliveira <edeiltonso@gmail.com>
2023-05-03 16:20:12 -0300

    feat: inicia implementação da reserva de memória pra string com zero

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ENTRADA "-.5+35.9+42^56/(-(-74-(+5^2+9)*2))-20"
+//#define ENTRADA "-.5+35.9+42^56/(-(-74-(+5^2+9)*2))-20"
 
 int hasSyntaxError(char exp[]) {
 
@@ -71,20 +71,35 @@ int hasSyntaxError(char exp[]) {
     return openedBrackets ? 1 : 0;
 }
 
-void addZeroToSpecialCases(char exp[]) {
+char * addZeroToSpecialCases(char exp[], char newExp[]) {
+    printf("\n>>> tamanho do endereco de newExp: %i bytes", sizeof(newExp));
+    printf("\n>>> tamanho do conteudo de newExp: %i bytes\n\n", sizeof((*newExp)));
+
     if (exp[0] == '.' || exp[0] == '+' || exp[0] == '-')
         printf("0%c", exp[0]);
-    
+
     int pos = 1;
     while (exp[pos] != '\0') {
         if (((exp[pos] == '+' || exp[pos] == '-') 
         && (exp[pos-1] < '0' || exp[pos-1] > '9') && exp[pos-1] != ')')
         || (exp[pos] == '.' && (exp[pos-1] < '0' || exp[pos-1] > '9')))
+        {
             printf("0%c", exp[pos]);
+            // aloca em newExp o espaço de um char
+            // adiciona '0' nesse espaço
+            // aloca em newExp o espaço de um char
+            // adiciona exp[0] nesse espaço
+        }
         else
+        {
             printf("%c", exp[pos]);
+            // aloca em newExp o espaço de um char
+            // adiciona exp[0] nesse espaço
+        }
         pos++;
     }
+
+    return newExp;
 }
 
 void transformCharToStruct(char vector) {
@@ -111,11 +126,19 @@ typedef struct {
 int main() {
     // if (hasSyntaxError(input)) exit(1);
 
-    printf("\n>>> tem erro? %s\n", hasSyntaxError(ENTRADA) ? "sim" : "nao");
+    char input[] = "-.5+35.9+42^56/(-(-74-(+5^2+9)*2))-20";
+    char newExp[] = "a";
 
-    printf(ENTRADA);
+    printf("\n>>> tem erro? %s\n", hasSyntaxError(input) ? "sim" : "nao");
+    printf(">>> tamanho: %i bytes (com char null)\n", sizeof(input)*sizeof(char));
+
+    printf(input);
     printf("\n");
-    addZeroToSpecialCases(ENTRADA);
+
+    addZeroToSpecialCases(input, newExp);
+
+    // printf(">>> tamanho do retorno: %i bytes\n", sizeof());
+
     // int pos = 0;
     // while (input[pos] != '\0') {
     //     printf("[%c] ", input[pos]);
