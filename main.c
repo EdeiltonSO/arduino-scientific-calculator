f62464fc5f4712a37df4040985242b2cd9f5820b
Edeilton Oliveira <edeiltonso@gmail.com>
2023-05-20 23:59:55 -0300

    draft: organiza função de conversão

--- main.c
+++ main.c
@@ -127,20 +127,20 @@ char * addZeroToSpecialCases(char exp[]) {
 
 void transformCharToStruct(char* exp) {
 
-    EXPRESSION_ELEMENT* elementList = (EXPRESSION_ELEMENT*) calloc(1, sizeof(EXPRESSION_ELEMENT));
+    int elementListSize = 1;
+    EXPRESSION_ELEMENT* elementList = (EXPRESSION_ELEMENT*) malloc(sizeof(EXPRESSION_ELEMENT));
 
     int pos = 0;
-    int elementListQtd = 0;
-    int sizeTemp = 1;
-    char* temp = (char*) calloc(sizeTemp, sizeof(char));
+    int currentNumberLength = 1;
+    char* currentNumber = (char*) malloc(sizeof(char));
     while (exp[pos] != '\0')
     {
         // .0123456789
         if (exp[pos] >= '0' && exp[pos] <= '9' || exp[pos] == '.') {
-            sizeTemp++;
-            temp = (char *) realloc(temp, sizeTemp);
-            temp[sizeTemp-2] = exp[pos];
-            temp[sizeTemp-1] = '\0';
+            currentNumberLength++;
+            currentNumber = (char *) realloc(currentNumber, currentNumberLength);
+            currentNumber[currentNumberLength-2] = exp[pos];
+            currentNumber[currentNumberLength-1] = '\0';
         }
         // +-*/^()
         else
@@ -148,65 +148,67 @@ void transformCharToStruct(char* exp) {
             EXPRESSION_ELEMENT number;
             EXPRESSION_ELEMENT symbol;
 
+            printf("\n>>> %s", currentNumber);
+
             // NUMBER
-            number.flags = 0b00000000;
+            number.flags = 0;
             int i = 0;
-            while (temp[i] != '\0')
+            while (currentNumber[i] != '\0')
             {
-                if (temp[i] == '.')
+                if (currentNumber[i] == '.')
                 {
-                    printf("aaaaaaaaaaa");
                     number.flags = 0b10000000;
                     break;
                 }
                 i++;
             }
 
-            if(number.flags |= (1 << 7)) {
+            if(number.flags & 1 << 7) {
                 printf("\neh decimal");
+                // converter de string pra double
+                number.content.number_double = 0.0;
             }
             else {
                 printf("\nnao eh decimal");
-            }
+                // converter de string pra int
+                number.content.number_int = 0;
+            }            
+            
+            // reseta currentNumber pra proxima iteração
+            currentNumberLength = 1;
+            currentNumber = (char *) realloc(currentNumber, currentNumberLength);
+            currentNumber[0] = '\0';
 
-            /*
-            if (number.decimal == 0b1)
+            // SYMBOL
+
+            symbol.flags = 0;
+            symbol.content.op = exp[pos];
+            
+            switch (exp[pos])
             {
-                number.content.number_double = temp;
+            case '(':
+            case ')':
+                symbol.flags |= 1 << 0;
+                break;
+            case '^':
+                symbol.flags |= 1 << 1;
+                break;
+            case '*':
+            case '/':
+                symbol.flags |= 1 << 1;
+                symbol.flags |= 1 << 0;
+                break;
+            default:
+                symbol.flags |= 1 << 2;
+                break;
             }
-            else {
-                number.content.number_int = temp;
-            }
-            */
-            
-            
-            // libera a memória de temp a partir do segundo elemento
-            sizeTemp = 1;
-            temp = (char *) realloc(temp, sizeTemp);
-            temp[0] = '\0';
-
-            // SYMBOL (daqui pra baio ta supostamente ok)
-
-            // symbol.decimal = 0;
-            // symbol.content.op = exp[pos];
-            // mudar p/ switch case
-
-            // if (exp[pos] == '(' || exp[pos] == ')')
-            //     symbol.priority = 1;
-            // else if (exp[pos] == '^')
-            //     symbol.priority = 2;
-            // else if (exp[pos] == '*' || exp[pos] == '/')
-            //     symbol.priority = 3;
-            // else
-            //     symbol.priority = 4;
 
             // alocar memoria e add os dois EXPRESSION_ELEMENT na elementList
-
         }
         
         pos++;
     }
-    
+    free(currentNumber);
 }
 
 void createRPNStack(/* recebe ponteiro pra pilha e array de structs */) {
@@ -223,7 +225,7 @@ int main() {
     char test[] = "0.5";
 
     char zero = '7';
-    transformCharToStruct("9+5");
+    transformCharToStruct("9345.67+124+444.5+");
     //printf("\n%d", zero-48);
     //printf(">>> %d", sizeof(EXPRESSION_ELEMENT));
 
