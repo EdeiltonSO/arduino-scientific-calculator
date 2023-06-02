258b62bbd6f30680a11b3e93d717519b3e729fc5
Edeilton Oliveira <edeiltonso@gmail.com>
2023-06-02 19:00:17 -0300

    test: possível solução para o bug dos endereços IP

--- test.c
+++ test.c
@@ -1,17 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double charToDouble(char charNum[]) {
-
-    double doubleNum = atof(charNum);
-
-    return doubleNum;
-}
+int hasSyntaxError(char *);
 
 int main() {
-    char num[] = "3.141867564534232456210";
-    double value = atof(num);
-    printf("Function: atof(\"%s\") = %e\n", num, value);
+    // char input[] = "-.5+35.9+42^56/(-(-74-(+5^2+9)*2))-20";
+
+    printf(">>> %d", hasSyntaxError("1+1+1.1.1+1"));
 
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNÇÕES PRONTAS
+
+int hasSyntaxError(char exp[]) {
+
+    if (exp[0] == '*' || exp[0] == '/' || 
+        exp[0] == '^' || exp[0] == ')' || 
+        exp[0] == '\0') return 1;
+
+    char pos = 1;
+    char openedBrackets = 0;
+    char dotsOfCurrentNumber = 0;
+
+    while (exp[pos] != '\0') {
+
+        if (exp[pos] >= '0' && exp[pos] <= '9' &&
+        (exp[pos-1] == ')' || exp[pos+1] == '(')
+        ) return 1;
+        
+        else if (exp[pos] == '.'
+        && (exp[pos+1] < '0' || exp[pos+1] > '9')
+        ) return 1;
+
+        else if (exp[pos] == '.') {
+
+            dotsOfCurrentNumber++; // PAREI AQUI MAS NAO TA FUNCIONANDO AINDA
+
+            // VEJA AÍ.
+
+            if (exp[pos+1] < '0' || exp[pos+1] > '9') return 1;
+        }
+
+        else if (exp[pos] == '+' || exp[pos] == '-')
+        {
+            dotsOfCurrentNumber = 0;
+            if ((exp[pos-1] < '0' || exp[pos-1] > '9')
+            && (exp[pos-1] < '(' || exp[pos-1] > ')')
+            || (exp[pos+1] < '0' || exp[pos+1] > '9')
+            && (exp[pos+1] != '(')
+            ) return 1;
+        }
+
+        else if (exp[pos] == '*' || exp[pos] == '/' || exp[pos] == '^')
+        {
+            dotsOfCurrentNumber = 0;
+            if ((exp[pos-1] < '0' || exp[pos-1] > '9')
+            && (exp[pos-1] != ')')
+            || (exp[pos+1] < '0' || exp[pos+1] > '9')
+            && (exp[pos+1] != '(')
+            ) return 1;
+        }
+
+        else if (exp[pos] == '(')
+        {
+            openedBrackets++;
+            dotsOfCurrentNumber = 0;
+            if (exp[pos-1] != '+' && exp[pos-1] != '-'
+            && exp[pos-1] != '*' && exp[pos-1] != '/'
+            && exp[pos-1] != '^' && exp[pos-1] != '('
+            || (exp[pos+1] < '0' || exp[pos+1] > '9')
+            && exp[pos+1] != '+' && exp[pos+1] != '-'
+            && exp[pos+1] != '('
+            ) return 1;
+        }
+
+        else if (exp[pos] == ')')
+        {
+            openedBrackets--;
+            dotsOfCurrentNumber = 0;
+            if ((exp[pos-1] < '0' || exp[pos-1] > '9') && exp[pos-1] != ')'
+            || exp[pos+1] != '+' && exp[pos+1] != '-'
+            && exp[pos+1] != '*' && exp[pos+1] != '/'
+            && exp[pos+1] != '^' && exp[pos+1] != ')'
+            ) return 1;
+        }
+
+        if (exp[pos+1] == '\0'
+            && (exp[pos] < '0' || exp[pos] > '9')
+            && exp[pos] != ')') return 1;
+
+        pos++;
+    }
+
+    printf("\ndotsOfCurrentNumber: %d\n", dotsOfCurrentNumber);
+    return (openedBrackets && (dotsOfCurrentNumber > 1)) ? 1 : 0;
 }
